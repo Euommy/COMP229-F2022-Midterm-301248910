62ad1285d7a386a5b67803825dd35c58e2340484
Euommy <104454973+Euommy@users.noreply.github.com>
2022-10-28 21:11:59 -0400

    Finalize the midterm

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,11 @@
 // Student Name: Eman Shalabi
 // Student ID: 301248910
 
-export const MongoURI = "mongodb://127.0.0.1:27017/media";
-export const Secret = "someSecret";
+// export const Secret = "MySecret";
+// //export const MongoURI = "mongodb://127.0.0.1:27017/media";
+// export const MongoURI = "mongodb+srv://Euomy:Suhail@123@cluster0.b2w7ygm.mongodb.net/media?retryWrites=true&w=majority";
+ export const Secret = "MySecret";
+ export const MongoURI = "mongodb+srv://thiago:thiago123@mongocluster.aoylqre.mongodb.net/media1?retryWrites=true&w=majority";
+
+//export const Secret = "MySecret";
+//export const MongoURI = "mongodb+srv://Eman:Suhail@123@cluster0.rplgkyy.mongodb.net/media2?retryWrites=true&w=majority";
